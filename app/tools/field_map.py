a7411e554a8a2b4783507d908db4eb855acec671
DennisSanntos <sined.marecas@gmail.com>
2025-07-27 06:26:59 -0300

    Create field_map.py

---
+++ app/tools/field_map.py
@@ -0,0 +1,27 @@
+# Tabela de reservas (Merge Dados Hits)
+FIELD_MAP_RESERVAS = {
+    "voucher": "field_5054798",
+    "nome_hospede": "field_5054799",
+    "telefone": "field_5054800",
+    "checkin": "field_5054842",
+    "checkout": "field_5054846",
+    "apartamento": "field_5054847",
+    "categoria_apartamento": "field_5054854",
+    "hospedes_apartamento": "field_5054855",
+    "email_hospede": "field_5054856",
+    "personalizacao_concluida": "field_5054864",
+    "dias_para_checkin": "field_5054865",
+    "link_chat": "field_5054866"
+}
+
+# Tabela de respostas do agente (preferencias_cafe)
+FIELD_MAP_PREFERENCIAS = {
+    "voucher": "field_5061153",
+    "frutas": "field_5061154",
+    "paes_salgados": "field_5061155",
+    "paes_sem_gluten": "field_5061156",
+    "acompanhamentos": "field_5061157",
+    "frios": "field_5061158",
+    "bolos_doces": "field_5061159",
+    "data_resposta": "field_5061160"
+}
