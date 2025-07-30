fd403a4156071653b75244b71959b2ba80ab13cf
DennisSanntos <sined.marecas@gmail.com>
2025-07-30 04:21:14 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+# app/__init__.py
+
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from .routes import main as main_bp
-    app.register_blueprint(main_bp)
+
+    from app.tools.routes import main
+    app.register_blueprint(main)
+
     return app
